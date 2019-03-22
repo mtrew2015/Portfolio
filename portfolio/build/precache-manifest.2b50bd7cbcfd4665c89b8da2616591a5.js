c0a8fc3f064596dafd00d414c5a469bac6731314
Michael Trew <mtrew2015@gmail.com>
2019-03-22 04:48:54 -0400

    adds react-router, changes styling to scss

---
+++ portfolio/build/precache-manifest.2b50bd7cbcfd4665c89b8da2616591a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c70992ab0b302b379db3",
+    "url": "/static/css/main.31b0b5f3.chunk.css"
+  },
+  {
+    "revision": "c70992ab0b302b379db3",
+    "url": "/static/js/main.bb75551f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "35c13f6d089558d52559aad6f0df3fe4",
+    "url": "/index.html"
+  }
+];
