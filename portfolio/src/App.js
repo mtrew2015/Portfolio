ec994747809b1e671125182fc9702100028cd945
Michael <40902786+mtrew2015@users.noreply.github.com>
2019-03-24 23:06:35 -0400

    CSS Reset, Renders Nav To App

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.scss';
+import React, { Component } from "react";
+import "./App.scss";
+import Nav from "./components/Nav/Nav";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Coming Soon....</h1>
+        <Nav />
       </div>
     );
   }
--- portfolio/src/components/Nav/Nav.js
+++ portfolio/src/components/Nav/Nav.js
@@ -1,1 +1,15 @@
+import React, { Component } from "react";
 import "./Nav.scss";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Michael Trew</h1>
+        <h2>Web Developer</h2>
+      </div>
+    );
+  }
+}
+
+export default Nav;
