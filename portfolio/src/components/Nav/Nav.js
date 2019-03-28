e92f1c1dd9e21a2d749d6dea0a1704be5de1b71b
Michael Trew <mtrew2015@gmail.com>
2019-03-28 06:39:16 -0400

    adds padding

--- portfolio/src/components/Nav/Nav.js
+++ portfolio/src/components/Nav/Nav.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 class Nav extends Component {
 	render() {
 		return (
-      <nav className="nav">
+      <div className="nav">
         <div className="menu">
 				<Link to ='/'>Home</Link>
 				<Link to='/about'>About</Link>
         <Link to='/projects'>Projects</Link>
         </div>
-			</nav>
+			</div>
 		);
 	}
 }
