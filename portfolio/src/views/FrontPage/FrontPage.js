2f2b3cf127d3cde924bd9506d20e9cf92c6264b3
Michael Trew <mtrew2015@gmail.com>
2019-04-06 02:19:21 -0400

    text styling

--- portfolio/src/views/FrontPage/FrontPage.js
+++ portfolio/src/views/FrontPage/FrontPage.js
@@ -7,8 +7,7 @@ class FrontPage extends Component {
 			<div className="body">
 				<div className="jumbotron">
 					<div className="textContainer">
-						<h1>Hello My Name Is Michael and I am a Web Developer</h1>
-						<h3>I look forward to helping you on your next project</h3>
+						<h2>Hello My Name is Michael and I am a Full-Stack Web Developer</h2>
 						<button>Contact Me</button>
 					</div>
 				</div>
