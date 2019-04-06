a5ee84864e470f1e9e427aaf12ef610603efc733
Michael Trew <mtrew2015@gmail.com>
2019-04-06 01:34:14 -0400

    working on font-size

--- portfolio/src/views/FrontPage/FrontPage.js
+++ portfolio/src/views/FrontPage/FrontPage.js
@@ -4,28 +4,28 @@ import './FrontPage.scss';
 class FrontPage extends Component {
 	render() {
 		return (
-			<div className='body'>
-				<div className='jumbotron'>
-					<div className='textContainer'>
+			<div className="body">
+				<div className="jumbotron">
+					<div className="textContainer">
 						<h1>Hello My Name Is Michael and I am a Web Developer</h1>
 						<h3>I look forward to helping you on your next project</h3>
 						<button>Contact Me</button>
 					</div>
 				</div>
 
-				<div className='featureContainer'>
+				<div className="featureContainer">
 					<h1>Development Done Better</h1>
-					<div className='boxContainer'>
-						<div className='box'>
+					<div className="boxContainer">
+						<div className="box">
 							<h2>Services To Meet Your Companies Needs</h2>
 						</div>
-						<div className='box'>
+						<div className="box">
 							<h2>Technologies Used To Best Suit Your Project</h2>
 						</div>
-						<div className='box'>
+						<div className="box">
 							<h2>Thoughtful Workflows</h2>
 						</div>
-						<div className='box'>
+						<div className="box">
 							<h2>Your Satisfaction Is Our Priority</h2>
 						</div>
 					</div>
